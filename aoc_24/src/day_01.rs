6c72a7b21187af06588b7eaca96fde934031a66b
Neal Scott <neal.scott@mckesson.com>
2024-12-05 10:47:50 -0600

    Finished with day 1 part 1

--- aoc_24/src/day_01.rs
+++ aoc_24/src/day_01.rs
@@ -1,15 +1,16 @@
 use regex;
+use utils::FileReader;
 
 pub struct Day01Processor(String);
 
 impl Day01Processor {
     fn new() -> Self {
-        // Self(String::from("./resources/day_01.txt"))
-        Self(String::from("./resources/aoc_24/sample_data.txt"))
+        Self(String::from("./resources/aoc_24/day_01.txt"))
+        // Self(String::from("./resources/aoc_24/sample_data.txt"))
     }
 
     pub fn process(&self) {
-        println!("Solving problems for day 01 using: {}", self.0)
+        PartOneProcessor::new(self.0.as_str()).process()
     }
 }
 
@@ -19,11 +20,81 @@ impl Default for Day01Processor {
     }
 }
 
-// #[allow(dead_code)]
+struct PartOneProcessor(String);
+
+impl PartOneProcessor {
+    fn new(s: &str) -> Self {
+        Self(s.to_string())
+    }
+
+    fn process(&self) {
+        let file_reader = FileReader::new(&self.0);
+
+        match PartOneValue::try_from(file_reader) {
+            Ok(p1_value) => println!("AoC 24 Day 01 Part 1: {}", p1_value.value()),
+            Err(msg) => println!("AoC 24 Day 01 Part 2: Failed with this message: {}", msg),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct PartOneValue(i32);
+
+impl PartOneValue {
+    fn value(&self) -> i32 {
+        self.0
+    }
+}
+
+impl TryFrom<FileReader> for PartOneValue {
+    type Error = crate::Error;
+
+    fn try_from(reader: FileReader) -> Result<Self, Self::Error> {
+        let input_nodes = reader
+            .read_lines()?
+            .map_while(Result::ok)
+            .map(InputNode::try_from)
+            .collect::<Result<Vec<InputNode>, crate::Error>>()?;
+
+        Ok(DistanceCalculator::new(input_nodes).calculate())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct DistanceCalculator {
+    nodes: Vec<InputNode>,
+}
+
+impl DistanceCalculator {
+    fn new(nodes: Vec<InputNode>) -> Self {
+        Self { nodes }
+    }
+
+    fn calculate(&self) -> PartOneValue {
+        let mut left = vec![];
+        let mut right = vec![];
+
+        for input_node in self.nodes.iter() {
+            left.push(input_node.left);
+            right.push(input_node.right);
+        }
+
+        left.sort();
+        right.sort();
+
+        let mut distance = 0i32;
+        for idx in 0..left.len() {
+            distance += (left[idx] - right[idx]).abs()
+        }
+
+        PartOneValue(distance)
+    }
+}
+
 #[derive(Debug, PartialEq)]
 struct InputNode {
-    pub left: u16,
-    pub right: u16,
+    pub left: i32,
+    pub right: i32,
 }
 
 impl TryFrom<String> for InputNode {
@@ -37,8 +108,8 @@ impl TryFrom<String> for InputNode {
         };
 
         let input_node = InputNode {
-            left: input_data["left"].parse::<u16>()?,
-            right: input_data["right"].parse::<u16>()?,
+            left: input_data["left"].parse::<i32>()?,
+            right: input_data["right"].parse::<i32>()?,
         };
 
         Ok(input_node)
@@ -57,6 +128,10 @@ impl TryFrom<String> for InputNode {
 mod tests {
     use super::*;
 
+    fn build_input_node(left: i32, right: i32) -> InputNode {
+        InputNode { left, right }
+    }
+
     #[test]
     fn input_node_from_string() {
         let expected = InputNode { left: 3, right: 4 };
@@ -65,5 +140,21 @@ mod tests {
             expected,
             InputNode::try_from(String::from("3   4")).unwrap()
         )
+    }
+
+    #[test]
+    fn distance_calculation() {
+        let distance_calculator = DistanceCalculator {
+            nodes: vec![
+                build_input_node(3, 4),
+                build_input_node(4, 3),
+                build_input_node(2, 5),
+                build_input_node(1, 3),
+                build_input_node(3, 9),
+                build_input_node(3, 3),
+            ],
+        };
+
+        assert_eq!(PartOneValue(11), distance_calculator.calculate())
     }
 }
