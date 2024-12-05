42e453e825c5bc98f3316fce55614e6af47c3658
Neal Scott <neal.scott@mckesson.com>
2024-12-05 13:25:27 -0600

    Added in AoC24 day 1 part 2

--- aoc_24/src/day_01.rs
+++ aoc_24/src/day_01.rs
@@ -1,4 +1,6 @@
+use itertools::Itertools;
 use regex;
+use std::collections::HashMap;
 use utils::FileReader;
 
 pub struct Day01Processor(String);
@@ -6,11 +8,11 @@ pub struct Day01Processor(String);
 impl Day01Processor {
     fn new() -> Self {
         Self(String::from("./resources/aoc_24/day_01.txt"))
-        // Self(String::from("./resources/aoc_24/sample_data.txt"))
     }
 
     pub fn process(&self) {
-        PartOneProcessor::new(self.0.as_str()).process()
+        PartOneProcessor::new(self.0.as_str()).process();
+        PartTwoProcessor::new(self.0.as_str()).process()
     }
 }
 
@@ -32,7 +34,7 @@ impl PartOneProcessor {
 
         match PartOneValue::try_from(file_reader) {
             Ok(p1_value) => println!("AoC 24 Day 01 Part 1: {}", p1_value.value()),
-            Err(msg) => println!("AoC 24 Day 01 Part 2: Failed with this message: {}", msg),
+            Err(msg) => println!("AoC 24 Day 01 Part 1: Failed with this message: {}", msg),
         }
     }
 }
@@ -91,6 +93,84 @@ impl DistanceCalculator {
     }
 }
 
+struct PartTwoProcessor(String);
+
+impl PartTwoProcessor {
+    fn new(s: &str) -> Self {
+        Self(s.to_string())
+    }
+
+    fn process(&self) {
+        let file_reader = FileReader::new(&self.0);
+
+        match PartTwoValue::try_from(file_reader) {
+            Ok(p2_value) => println!("AoC 24 Day 01 Part 2: {}", p2_value.value()),
+            Err(msg) => println!("AoC 24 Day 01 Part 2: Failed with this message: {}", msg),
+        }
+    }
+}
+
+impl TryFrom<FileReader> for PartTwoValue {
+    type Error = crate::Error;
+
+    fn try_from(reader: FileReader) -> Result<Self, Self::Error> {
+        let input_nodes = reader
+            .read_lines()?
+            .map_while(Result::ok)
+            .map(InputNode::try_from)
+            .collect::<Result<Vec<InputNode>, crate::Error>>()?;
+
+        SimilarityCalculator::new(input_nodes).calculate()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct PartTwoValue(i32);
+
+impl PartTwoValue {
+    fn value(&self) -> i32 {
+        self.0
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct SimilarityCalculator {
+    left_values: Vec<i32>,
+    right_groups: HashMap<i32, Vec<i32>>,
+}
+
+impl SimilarityCalculator {
+    fn new(input_nodes: Vec<InputNode>) -> Self {
+        let mut left_nodes = vec![];
+        let mut right_nodes = vec![];
+
+        for input_node in input_nodes.iter() {
+            left_nodes.push(input_node.left);
+            right_nodes.push(input_node.right);
+        }
+
+        Self {
+            left_values: left_nodes,
+            right_groups: right_nodes.into_iter().into_group_map_by(|x| *x),
+        }
+    }
+
+    fn calculate(&self) -> Result<PartTwoValue, crate::Error> {
+        let mut total = 0;
+
+        for value in self.left_values.iter() {
+            let score = match self.right_groups.get(value) {
+                Some(matches) => matches.len().try_into()?,
+                None => 0i32,
+            };
+
+            total += value * score;
+        }
+
+        Ok(PartTwoValue(total))
+    }
+}
+
 #[derive(Debug, PartialEq)]
 struct InputNode {
     pub left: i32,
@@ -115,14 +195,6 @@ impl TryFrom<String> for InputNode {
         Ok(input_node)
     }
 }
-
-// Sample Data:
-//  3   4
-//  4   3
-//  2   5
-//  1   3
-//  3   9
-//  3   3
 
 #[cfg(test)]
 mod tests {
@@ -156,5 +228,20 @@ mod tests {
         };
 
         assert_eq!(PartOneValue(11), distance_calculator.calculate())
+    }
+
+    #[test]
+    fn similarity_calculation() {
+        let input_nodes = vec![
+            build_input_node(3, 4),
+            build_input_node(4, 3),
+            build_input_node(2, 5),
+            build_input_node(1, 3),
+            build_input_node(3, 9),
+            build_input_node(3, 3),
+        ];
+        let similarity_calculator = SimilarityCalculator::new(input_nodes);
+
+        assert_eq!(PartTwoValue(31), similarity_calculator.calculate().unwrap())
     }
 }
