10cdc0761ed04b621e43f1d064df231f79c906af
Neal Scott <neal.scott@mckesson.com>
2024-12-11 16:04:17 -0600

    Solution for part 2

--- aoc_24/src/day_02.rs
+++ aoc_24/src/day_02.rs
@@ -6,13 +6,12 @@ pub struct Day02Processor(String);
 
 impl Day02Processor {
     fn new() -> Self {
-        // Self(String::from("./resources/aoc_24/sample_data.txt"))
         Self(String::from("./resources/aoc_24/day_02.txt"))
     }
 
     pub fn process(&self) {
-        // println!("Running Day02 Stuff for: {}", self.0)
         PartOneProcessor::new(self.0.as_str()).process();
+        PartTwoProcessor::new(self.0.as_str()).process();
     }
 }
 
@@ -67,6 +66,51 @@ impl TryFrom<FileReader> for PartOneValue {
     }
 }
 
+struct PartTwoProcessor(String);
+
+impl PartTwoProcessor {
+    fn new(s: &str) -> Self {
+        Self(s.to_string())
+    }
+
+    fn process(&self) {
+        let file_reader = FileReader::new(&self.0);
+
+        match PartTwoValue::try_from(file_reader) {
+            Ok(p2_value) => println!("AoC 24 Day 02 Part 2: {}", p2_value.value()),
+            Err(msg) => println!("AoC 24 Day 02 Part 2: Failed with this message: {}", msg),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct PartTwoValue(i16);
+
+impl PartTwoValue {
+    fn value(&self) -> i16 {
+        self.0
+    }
+}
+
+impl TryFrom<FileReader> for PartTwoValue {
+    type Error = crate::Error;
+
+    fn try_from(reader: FileReader) -> Result<Self, Self::Error> {
+        let reactor_reports = reader
+            .read_lines()?
+            .map_while(Result::ok)
+            .map(ReactorReport::try_from)
+            .collect::<Result<Vec<ReactorReport>, crate::Error>>()?;
+
+        let safe_reports: Vec<ReactorReport> = reactor_reports
+            .into_iter()
+            .filter(|rr| rr.is_dampened_safe())
+            .collect();
+
+        Ok(PartTwoValue(i16::try_from(safe_reports.len())?))
+    }
+}
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 struct Level(i16);
 
@@ -102,6 +146,22 @@ impl ReactorReport {
         }
 
         true
+    }
+
+    fn is_dampened_safe(&self) -> bool {
+        let is_safe = self.is_safe() || {
+            for idx in 0..self.levels.len() {
+                let mut levels = self.levels.clone();
+                levels.remove(idx);
+
+                if (ReactorReport { levels }).is_safe() {
+                    return true;
+                }
+            }
+            false
+        };
+
+        is_safe
     }
 }
 
@@ -169,6 +229,13 @@ mod tests {
     }
 
     #[test]
+    fn report_dampened_unsafe_increase() {
+        assert!(!ReactorReport::try_from("1 2 7 8 9".to_string())
+            .unwrap()
+            .is_dampened_safe())
+    }
+
+    #[test]
     fn report_unsafe_increase_and_decrease() {
         assert!(!ReactorReport::try_from("1 3 2 4 5".to_string())
             .unwrap()
@@ -176,9 +243,23 @@ mod tests {
     }
 
     #[test]
+    fn report_dampened_safe_increase_and_decrease() {
+        assert!(ReactorReport::try_from("1 3 2 4 5".to_string())
+            .unwrap()
+            .is_dampened_safe())
+    }
+
+    #[test]
     fn report_unsafe_no_change_in_level() {
         assert!(!ReactorReport::try_from("8 6 4 4 1".to_string())
             .unwrap()
             .is_safe())
+    }
+
+    #[test]
+    fn report_dampened_safe_no_change_in_level() {
+        assert!(ReactorReport::try_from("8 6 4 4 1".to_string())
+            .unwrap()
+            .is_dampened_safe())
     }
 }
