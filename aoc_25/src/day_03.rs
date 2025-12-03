7d542ff83acf87b12c841829c04d74755aab282a
Neal Scott <neal.scott@mckesson.com>
2025-12-03 15:23:02 -0600

    AoC 25 day 3 part 2

--- aoc_25/src/day_03.rs
+++ aoc_25/src/day_03.rs
@@ -11,6 +11,7 @@ impl Day03Processor {
 
     pub fn process(&self) {
         PartOneProcessor::new(self.0.as_str()).process();
+        PartTwoProcessor::new(self.0.as_str()).process();
     }
 }
 
@@ -67,6 +68,52 @@ impl TryFrom<FileReader> for PartOneValue {
     }
 }
 
+struct PartTwoProcessor(String);
+
+impl PartTwoProcessor {
+    fn new(s: &str) -> Self {
+        Self(s.to_string())
+    }
+
+    fn process(&self) {
+        let file_reader = FileReader::new(&self.0);
+
+        match PartTwoValue::try_from(file_reader) {
+            Ok(p2_value) => println!("AoC 25 Day 03 Part 2: {}", p2_value.get()),
+            Err(msg) => println!("AoC 25 Day 03 Part 2: Failed with message: {}", msg),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct PartTwoValue(i64);
+
+impl PartTwoValue {
+    fn get(&self) -> i64 {
+        self.0
+    }
+}
+
+impl TryFrom<FileReader> for PartTwoValue {
+    type Error = crate::Error;
+
+    fn try_from(reader: FileReader) -> Result<Self, Self::Error> {
+        let battery_banks = reader
+            .read_lines()?
+            .map_while(Result::ok)
+            .map(BatteryBank::try_from)
+            .collect::<Result<Vec<BatteryBank>, crate::Error>>()?;
+
+        let all_joltages: Vec<i64> = battery_banks
+            .into_iter()
+            .map(|bb| bb.overridden_joltage())
+            .collect();
+
+        let total_joltage = all_joltages.into_iter().sum();
+
+        Ok(PartTwoValue(total_joltage))
+    }
+}
 #[derive(Debug, PartialEq)]
 struct BatteryBank(Vec<i8>);
 
@@ -100,6 +147,18 @@ impl BatteryBank {
 
         joltage
     }
+
+    fn overridden_joltage(&self) -> i64 {
+        let mut tracker = JoltageTracker::new(12, self.0.len() as i8);
+        let mut battery_position = 0;
+
+        for battery_value in self.0.clone().into_iter() {
+            battery_position += 1;
+            tracker.adjust_joltage(battery_value, battery_position);
+        }
+
+        tracker.joltage()
+    }
 }
 
 impl TryFrom<String> for BatteryBank {
@@ -114,6 +173,63 @@ impl TryFrom<String> for BatteryBank {
             .collect::<Result<Vec<i8>, std::num::ParseIntError>>()?;
 
         Ok(Self(batteries))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct JoltageTracker {
+    tracked_joltage: Vec<i8>,
+    joltage_size: i8,
+    possible_batteries: i8,
+}
+
+impl JoltageTracker {
+    fn new(joltage_size: i8, possible_batteries: i8) -> Self {
+        Self {
+            tracked_joltage: vec![],
+            joltage_size,
+            possible_batteries,
+        }
+    }
+
+    fn adjust_joltage(&mut self, battery_value: i8, battery_position: i8) {
+        let mut joltage_position: usize = 0;
+        let remaining_batteries = self.possible_batteries - battery_position;
+
+        for jolt in self.tracked_joltage.clone().into_iter() {
+            joltage_position += 1;
+
+            if battery_value > jolt
+                && remaining_batteries >= self.joltage_size - joltage_position as i8
+            {
+                self.tracked_joltage[joltage_position - 1] = battery_value;
+                self.tracked_joltage.truncate(joltage_position);
+                return;
+            }
+        }
+
+        if joltage_position < self.joltage_size as usize {
+            self.tracked_joltage.push(battery_value);
+        }
+    }
+
+    fn joltage(&self) -> i64 {
+        let Some(string_representation) = self
+            .tracked_joltage
+            .clone()
+            .into_iter()
+            .map(|j| j.to_string())
+            .reduce(|acc, s| acc + &s)
+        else {
+            // There should always be something here.
+            return 0;
+        };
+
+        if let Ok(joltage) = string_representation.parse::<i64>() {
+            return joltage;
+        } else {
+            return 0;
+        }
     }
 }
 
@@ -169,5 +285,64 @@ mod tests {
         let reader = FileReader::new(SAMPLE_FILE);
 
         assert_eq!(357, PartOneValue::try_from(reader).unwrap().get())
+    }
+
+    #[test]
+    fn test_adjust_joltage1() {
+        let mut tracker = JoltageTracker::new(12, 15);
+        let batteries = vec![2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8];
+
+        let mut position = 0;
+        for battery in batteries.into_iter() {
+            position += 1;
+            tracker.adjust_joltage(battery, position);
+        }
+
+        let expected = JoltageTracker {
+            tracked_joltage: vec![4, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8],
+            joltage_size: 12,
+            possible_batteries: 15,
+        };
+
+        assert_eq!(expected, tracker)
+    }
+
+    #[test]
+    fn test_adjust_joltage2() {
+        let mut tracker = JoltageTracker::new(12, 15);
+        let batteries = vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1];
+
+        let mut position = 0;
+        for battery in batteries.into_iter() {
+            position += 1;
+            tracker.adjust_joltage(battery, position);
+        }
+
+        let expected = JoltageTracker {
+            tracked_joltage: vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1],
+            joltage_size: 12,
+            possible_batteries: 15,
+        };
+
+        assert_eq!(expected, tracker)
+    }
+
+    #[test]
+    fn test_joltage() {
+        let tracker = JoltageTracker {
+            tracked_joltage: vec![4, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8],
+            joltage_size: 12,
+            possible_batteries: 15,
+        };
+
+        assert_eq!(434234234278, tracker.joltage())
+    }
+
+    #[test]
+    fn test_part_two_value_from_file_reader() {
+        let reader = FileReader::new(SAMPLE_FILE);
+        let p2_value = PartTwoValue::try_from(reader);
+
+        assert_eq!(3121910778619, p2_value.unwrap().get())
     }
 }
