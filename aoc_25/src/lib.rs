98b7cc4d0b986cc1ab2dbbafdc5d29efb33360bc
Neal Scott <neal.scott@mckesson.com>
2025-12-05 08:41:11 -0600

    AoC25 day 4 part 1

--- aoc_25/src/lib.rs
+++ aoc_25/src/lib.rs
@@ -1,6 +1,7 @@
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
 mod error;
 
 pub use error::Error;
@@ -8,6 +9,7 @@ pub use error::Error;
 use day_01::Day01Processor;
 use day_02::Day02Processor;
 use day_03::Day03Processor;
+use day_04::Day04Processor;
 
 pub struct AoC25Processor {}
 
@@ -16,5 +18,6 @@ impl AoC25Processor {
         Day01Processor::default().process();
         Day02Processor::default().process();
         Day03Processor::default().process();
+        Day04Processor::default().process();
     }
 }
